17f32d8d4cb000fe9a648c66c95fb88782dfe013
Dong <asia123dong@gmail.com>
2019-12-03 16:28:59 +0800

    Implementation the function.

---
+++ src/main/java/com/citic/bank/service/serviceImp/FundInfoImp.java
@@ -0,0 +1,27 @@
+package com.citic.bank.service.serviceImp;
+
+import com.citic.bank.dao.TradeMapper;
+import com.citic.bank.model.Trade;
+import com.citic.bank.service.FundInfo;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FundInfoImp implements FundInfo {
+    @Autowired
+    private TradeMapper tradeMapper;
+
+    @Override
+    public List<Trade> getAllTrade() {
+        List result=tradeMapper.getAllTrades();
+        return result;
+    }//Of fun getAllTrade
+
+    @Override
+    public Trade getById(String id) {
+        return tradeMapper.selectByPrimaryKey(Long.parseLong(id));
+    }
+}

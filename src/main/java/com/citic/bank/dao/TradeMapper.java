77586d48b5fd0d8d40a16db9698eadc4484eeb79
Dong <asia123dong@gmail.com>
2019-12-03 16:26:39 +0800

    Implementation the function.

--- src/main/java/com/citic/bank/dao/TradeMapper.java
+++ src/main/java/com/citic/bank/dao/TradeMapper.java
@@ -2,7 +2,11 @@ package com.citic.bank.dao;
 
 import com.citic.bank.model.Trade;
 import com.citic.bank.model.TradeExample;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface TradeMapper {
     long countByExample(TradeExample example);
@@ -22,4 +26,7 @@ public interface TradeMapper {
     int updateByPrimaryKeySelective(Trade record);
 
     int updateByPrimaryKey(Trade record);
+
+    @Select("select * from order_info")
+    List<Trade> getAllTrades();
 }

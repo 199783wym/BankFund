6f5c39296a0e122b3ca58bf598e934f4b705b689
Dong <asia123dong@gmail.com>
2019-12-03 10:20:48 +0800

    Fund sell transactions and individual info.

---
+++ src/main/java/com/citic/bank/controller/FundSellController.java
@@ -0,0 +1,10 @@
+/**
+ * @Author XXD
+ *
+ */
+package com.citic.bank.controller;
+
+public class FundSellController {
+
+
+}//Of class FundSellController
---
+++ src/main/java/com/citic/bank/controller/IndividualCenterController.java
@@ -0,0 +1,36 @@
+/**
+ * @Author XXD
+ */
+package com.citic.bank.controller;
+
+import com.citic.bank.model.User;
+import com.citic.bank.service.UserInfoUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/personal")
+public class IndividualCenterController {
+    @Autowired
+    private UserInfoUpdate userInfoUpdate;
+
+    @RequestMapping("")
+    public String personal() {
+        return "personal";
+    }//Of personal
+
+    @RequestMapping("/fund")
+
+    @ResponseBody
+    public String getFund() {
+
+        List<User> allUsers = userInfoUpdate.showAllUsers();
+
+        return "success";
+    }//Of getFund
+
+}//Of class IndividualCenterController

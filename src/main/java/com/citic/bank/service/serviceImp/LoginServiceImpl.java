8b36114f7b8580cc5463b8f71f0bd7ea0caad7bc
wangym <642688028@qq.com>
2019-12-04 17:18:58 +0800

    session 登录功能

---
+++ src/main/java/com/citic/bank/service/serviceImp/LoginServiceImpl.java
@@ -0,0 +1,9 @@
+package com.citic.bank.service.serviceImp;
+
+/**
+ * @Author: ym
+ * @Date: 2019/12/4 16:51
+ * @Version 1.0
+ */
+public class LoginServiceImpl {
+}
---
+++ src/main/java/com/citic/bank/util/UserSession.java
@@ -0,0 +1,9 @@
+package com.citic.bank.util;
+
+/**
+ * @Author: ym
+ * @Date: 2019/12/4 16:30
+ * @Version 1.0
+ */
+public class UserSession {
+}

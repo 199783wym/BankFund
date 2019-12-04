7a4776da035796a56d2279451ff190a9587d4df4
wangym <642688028@qq.com>
2019-12-04 17:20:06 +0800

    session 登录后台

--- src/main/java/com/citic/bank/controller/LoginController.java
+++ src/main/java/com/citic/bank/controller/LoginController.java
@@ -1,6 +1,14 @@
 package com.citic.bank.controller;
 
+import com.citic.bank.dao.UserMapper;
+import com.citic.bank.model.User;
+import com.citic.bank.service.LoginService;
+import com.citic.bank.util.Md5;
+import com.citic.bank.util.UserSession;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,13 +19,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private LoginService loginService;
+
     @RequestMapping("/tologin")
     public String tologin(){
         return "loginPage.jsp";
     }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login(@Param(value="number")String number,
+                        @Param("password")String password,
+                        Model model){
+         User user=new User();
+         user.setAccountPhone(number);
+         user.setAccountPwd(Md5.MD5(password.replace(" ","").trim()));
+         if(user.getId()!=null){
+             UserSession.putUserSession(user);
+         }else{
+             model.addAttribute("success", "登录失败");
+             return "successBuy.jsp";
+         }
         return "index.jsp";
     }
 }
--- src/main/java/com/citic/bank/service/serviceImp/LoginServiceImpl.java
+++ src/main/java/com/citic/bank/service/serviceImp/LoginServiceImpl.java
@@ -1,9 +1,32 @@
 package com.citic.bank.service.serviceImp;
 
+import com.citic.bank.dao.UserMapper;
+import com.citic.bank.model.User;
+import com.citic.bank.model.UserExample;
+import com.citic.bank.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @Author: ym
  * @Date: 2019/12/4 16:51
  * @Version 1.0
  */
-public class LoginServiceImpl {
+@Service
+public class LoginServiceImpl implements LoginService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User Login(User user) {
+       UserExample userExample= new UserExample();
+        userExample.createCriteria().andAccountPhoneEqualTo(user.getAccountPhone()).andAccountPwdEqualTo(user.getAccountPwd());
+        List<User> users= userMapper.selectByExample(userExample);
+        if(users.size()!=0){
+            user=users.get(0);
+        }
+        return user;
+    }
 }

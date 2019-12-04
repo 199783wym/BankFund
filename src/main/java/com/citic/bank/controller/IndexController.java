a229f6a1d0228dde92034ac816e17fe4cb7e3b53
wangym <642688028@qq.com>
2019-12-04 16:19:25 +0800

    给前端页面传模糊搜索结果

--- src/main/java/com/citic/bank/controller/IndexController.java
+++ src/main/java/com/citic/bank/controller/IndexController.java
@@ -4,12 +4,17 @@ import com.citic.bank.dto.ProductDTO;
 import com.citic.bank.model.Product;
 
 import com.citic.bank.service.ProductService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -34,6 +39,41 @@ public class IndexController {
         model.addAttribute("productList3", productList3);
         model.addAttribute("productList4", productList4);
         model.addAttribute("productList5", productList5);
+        Collections.sort(productList1,new Comparator<ProductDTO>() {
+            @Override
+            public int compare(ProductDTO p1, ProductDTO p2) {
+                if(p1.getDay()< p2.getDay()){
+                    return 1;
+                }
+                if(p1.getDay() == p2.getDay()){
+                    return 0;
+                }
+                return -1;
+            }
+        });
+        model.addAttribute("productList6", productList1);
         return "index.jsp";
+    }
+
+    @RequestMapping("/search")
+    public String searchName(@Param(value="searchName")String name,
+                             Model model){
+        List<ProductDTO> productList1= productService.queryProductAll();
+        List<ProductDTO> productDTOS=productService.query(name);
+        Collections.sort(productList1,new Comparator<ProductDTO>() {
+            @Override
+            public int compare(ProductDTO p1, ProductDTO p2) {
+                if(p1.getDay()< p2.getDay()){
+                    return 1;
+                }
+                if(p1.getDay() == p2.getDay()){
+                    return 0;
+                }
+                return -1;
+            }
+        });
+        model.addAttribute("productList6", productList1);
+        model.addAttribute("productList1",productDTOS);
+        return "index";
     }
 }
--- src/main/java/com/citic/bank/controller/LoginController.java
+++ src/main/java/com/citic/bank/controller/LoginController.java
@@ -1,9 +1,23 @@
 package com.citic.bank.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Author: ym
  * @Date: 2019/12/4 14:56
  * @Version 1.0
  */
+@Controller
 public class LoginController {
+
+    @RequestMapping("/tologin")
+    public String tologin(){
+        return "loginPage.jsp";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "index.jsp";
+    }
 }
